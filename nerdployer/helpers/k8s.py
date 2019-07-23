50212f11d41d01ef5da6c4b08795e5f95d56d08d
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2019-07-23 17:15:30 -0300

    changes according to review

--- nerdployer/helpers/k8s.py
+++ nerdployer/helpers/k8s.py
@@ -4,21 +4,22 @@ import subprocess
 class K8s(object):
 
     def __init__(self, server, token, namespace, opts):
-        self.server = server
-        self.token = token
-        self.namespace = namespace
-        self.opts = opts
+        self.base_cmd = 'kubectl --server={server} --token={token}' \
+                        ' -n {namespace} {opts} '.format(server=server,
+                                                         token=token,
+                                                         namespace=namespace,
+                                                         opts=opts)
 
     def apply(self, template):
-        apply_command = 'cat <<EOF | kubectl --server={server} --token={token} {namespace} {opts} ' \
+        apply_command = 'cat <<EOF | {base_cmd} --record' \
                         'apply -f -\n{template}\nEOF'.format(
-            server=self.server, token=self.token, template=template, opts=self.opts, namespace=self.namespace)
+            template=template, base_cmd=self.base_cmd)
         return self._run_sub_process(apply_command)
 
     def rollout_status(self, deployment):
-        check_command = 'kubectl --server={server} --token={token} {namespace} ' \
+        check_command = '{base_cmd} ' \
                         'rollout status deployment {deployment}'.format(
-            server=self.server, token=self.token, deployment=deployment, namespace=self.namespace)
+            deployment=deployment, base_cmd=self.base_cmd)
         return self._run_sub_process(check_command)
 
     def _run_sub_process(self, command):
